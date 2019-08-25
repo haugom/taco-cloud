29d0572cb18187d8ece6e3e41dc00c406767bdd3
Geir Haugom <ghagit@haugom.org>
2019-08-25 12:50:40 +0200

    Create simple hello world controller

---
+++ src/test/java/tacos/HomeControllerTest.kt
@@ -0,0 +1,33 @@
+package tacos
+
+import org.hamcrest.Matchers.containsString
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+@RunWith(SpringRunner::class)
+@WebMvcTest(HomeController::class)
+class HomeControllerTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc;
+
+    @Test
+    public fun testHomePage() {
+        mockMvc.perform(get("/"))
+
+                .andExpect(status().isOk())
+
+                .andExpect(view().name("home"))
+
+                .andExpect(content().string(
+                        containsString("Welcome to...")
+                ))
+    }
+
+}
